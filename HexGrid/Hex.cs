7940f0660bd91de394847bf845c38b5756331a9d
kirjh <71944013+kirjh@users.noreply.github.com>
2025-12-26 00:41:21 -0800

    init hexgrid class and unit tests

---
+++ HexGrid/Hex.cs
@@ -0,0 +1,12 @@
+﻿namespace HexGrid;
+
+public class Hex
+{
+  public int q { get; }
+  public int r { get; }
+
+  public Hex(int q, int r) {
+    this.q = q;
+    this.r = r;
+  }
+}
---
+++ HexTests/HexClassTests.cs
@@ -0,0 +1,15 @@
+﻿namespace HexTests;
+using HexGrid;
+
+[TestClass]
+public sealed class HexClassTests 
+{
+
+  // Test constructor with standard values
+  [TestMethod]
+  public void TestClassConstructor1() {
+    Hex hex = new Hex(1, 2);
+
+    Assert.IsTrue(hex.q == 1, "q was not set correctly");
+  }
+}
