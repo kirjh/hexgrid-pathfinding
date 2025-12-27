59e09ff19db9d3a155c849c4c020320b85b12be5
kirjh <71944013+kirjh@users.noreply.github.com>
2025-12-26 18:07:24 -0800

    implement class overrides

--- HexGrid/Hex.cs
+++ HexGrid/Hex.cs
@@ -2,11 +2,31 @@
 
 public class Hex
 {
-  public int q { get; }
-  public int r { get; }
+  private int q { get; set; }
+  private int r { get; set; }
+  public int Q { get { return this.q; } }
+  public int R { get { return this.r; } }
+  public int S { get { return - this.q - this.r; } }
 
-  public Hex(int q, int r) {
+  // Axial Constructor
+  public Hex(int q, int r)
+  {
     this.q = q;
     this.r = r;
+  }
+
+  // Overrides
+  public override string ToString() {
+    return $"[{this.Q}, {this.R}, {this.S}]";
+  }
+  public override bool Equals(object obj)
+  {
+    if (obj is not Hex hex) return false;
+
+    return this.q == hex.q && this.r == hex.r;
+  }
+  public override int GetHashCode()
+  {
+    return (this.q, this.r).GetHashCode();
   }
 }
--- HexTests/HexClassTests.cs
+++ HexTests/HexClassTests.cs
@@ -10,6 +10,30 @@ public sealed class HexClassTests
   public void TestClassConstructor1() {
     Hex hex = new Hex(1, 2);
 
-    Assert.IsTrue(hex.q == 1, "q was not set correctly");
+    Assert.IsTrue(hex.Q + hex.R + hex.S == 0, "value s incorrect; q + r + s should sum to 0");
+  }
+  // Test constructor with standard values
+  [TestMethod]
+  public void TestEquality1() {
+    Hex a = new Hex(1, 2);
+    Hex b = new Hex(2, 1);
+
+    Assert.IsTrue(!a.Equals(b), "value s incorrect; q + r + s should sum to 0");
+  }
+  // Test HashCode
+  [TestMethod]
+  public void TestHash1() {
+    Hex a = new Hex(1, 2);
+    Hex b = new Hex(1, 2);
+
+    Assert.IsTrue(a.GetHashCode() == b.GetHashCode(), "value s incorrect; q + r + s should sum to 0");
+  }
+  // Test HashCode
+  [TestMethod]
+  public void TestHash2() {
+    Hex a = new Hex(1, 2);
+    Hex b = new Hex(2, 1);
+
+    Assert.IsTrue(a.GetHashCode() != b.GetHashCode(), "value s incorrect; q + r + s should sum to 0");
   }
 }
